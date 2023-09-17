7dd89c2d002f6089b05d27f6b243296aa3936e51
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-09-17 07:49:04 -0700

    add files

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT ||8080;
+const port = process.env.PORT || 8080;
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
 app.listen(port, () => {
-	console.log("Node application listening on port "+port);
-}); 
+  console.log("Node application listening on port " + port);
+});
