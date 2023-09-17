ba97e5bde2452752796341f1a7ee0044e190159d
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-09-17 08:58:22 -0700

    update static files

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const path = require('path');
+
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
+app.use('/js', express.static(path.join(__dirname, 'js')));
 
 app.listen(port, () => {
   console.log("Node application listening on port " + port);
